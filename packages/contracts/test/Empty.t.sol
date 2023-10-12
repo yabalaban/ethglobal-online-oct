e8151bf90fca626191b8b0d4ee005ed3d0c6d651
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-10-13 00:49:55 +0100

    uma promise contract

---
+++ packages/contracts/test/Empty.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract EmptyTest is Test {   
+    function setUp() public {}
+}
